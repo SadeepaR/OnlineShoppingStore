07a659b4d7ae1a71d0d5cd1a72894c6ab19221ad
amithaisuru <rkamithaisuru@gmail.com>
2025-07-17 17:06:24 +0530

    init commit

---
+++ email_management/src/main/java/com/example/email_management/EmailManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailManagementApplication.class, args);
+	}
+
+}
---
+++ email_management/src/main/java/com/example/email_management/service/EmailService.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/email_management/service/EmailService.java
+package com.example.email_management.service;
+import com.example.email_management.dto.EmailRequest;
+
+import jakarta.mail.MessagingException;
+
+public class EmailService {
+
+    public void sendEmail(EmailRequest request) throws MessagingException {
+
+    }
+}
