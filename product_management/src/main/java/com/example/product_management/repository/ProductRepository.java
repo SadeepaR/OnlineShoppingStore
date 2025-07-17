39aab723fee4e3d4e7a708e46ef37cd12020c49a
Sadeepa Ranasinghe <sadeeparanasinghe2000@gmail.com>
2025-07-17 23:48:35 +0530

    product service

--- product_management/src/main/java/com/example/product_management/repository/ProductRepository.java
+++ product_management/src/main/java/com/example/product_management/repository/ProductRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.product_management.model.Product;
 
+
 public interface ProductRepository extends JpaRepository<Product, Long> {}
 // This interface extends JpaRepository to provide CRUD operations for Product entities
